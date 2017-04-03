f4152a851891e44cfa7606867bd60c2f2b8748a2
Andreas Nilsson <and.nil@outlook.com>
2017-04-03 06:48:17 +0200

    Update auth.ts

--- createleads/auth.ts
+++ createleads/auth.ts
@@ -1,8 +1,8 @@
 export const auth = {
-        username: "andreas@datingautomation.onmicrosoft.com",
-        password: "Letsgorangers!",
-        client_id: "eece4113-a0c3-434e-b060-4795984c6276",
-        client_secret: "lUoxdvkO0PIC9HRubSh7XslteI1bHAajK9I4TU2rmto=",
-        resource: "https://datingautomation.crm4.dynamics.com",
-        commonAuthority: "https://login.windows.net/datingautomation.onmicrosoft.com/oauth2/token",
+        username: "",
+        password: "",
+        client_id: "",
+        client_secret: "",
+        resource: "https://<<tenant>>.crm4.dynamics.com",
+        commonAuthority: "https://login.windows.net/<<tenant>>.onmicrosoft.com/oauth2/token",
     }
