82675e2012c7c15f16e48329e9ac4aeb534ca45b
Unknown <and.nil@outlook.com>
2017-04-03 06:42:27 +0200

    removed tracking of settings file

--- createleads/auth.ts
+++
@@ -1,8 +0,0 @@
-export const auth = {
-        username: "",
-        password: "",
-        client_id: "",
-        client_secret: "",
-        resource: "https://<<tenant>>.crm4.dynamics.com",
-        commonAuthority: "https://login.windows.net/<<tenant>>.onmicrosoft.com/oauth2/token",
-    }
